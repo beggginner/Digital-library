caa649faade176b08ccd552e68d6c4b25a597864
Tharun <53267275+tharun634@users.noreply.github.com>
2020-06-11 18:41:47 +0530

    plays sound when anyone of the timer hits zero

--- MainActivity.java
+++ MainActivity.java
@@ -1,181 +1,214 @@
 package com.example.timer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.NavUtils;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final long START_TIME_IN_MILLIS = 600000;
+    Integer totalTime;
+    ConstraintLayout welcomePage;
+    ConstraintLayout clockPage;
+    EditText minutes;
+    Button playButton;
+    Button startButton;
+    TextView whiteCounterTextView;
+    TextView blackCounterTextView;
+    Button wSwitchButton;
+    Button bSwitchButton;
+    Button reset;
 
-    private TextView mTextViewCountdown;
-    private TextView mTextViewCountdown2;
-    private Button mButtonStartPause;
-    private Button mButtonStartPause2;
-    private Button mButtonReset;
+    CountDownTimer whiteCountDownTimer;
+    CountDownTimer blackCountDownTimer;
 
-    private CountDownTimer mCountDownTimer;
-    private CountDownTimer mCountDownTimer2;
-
-    private boolean mTimer1Running;
-    private boolean mTimer2Running;
-
-    private long mTimeLeft1InMillis = START_TIME_IN_MILLIS;
-    private long mTimeLeft2InMillis = START_TIME_IN_MILLIS;
+    long wtimeleftinmillis = 60000;
+    long btimeleftinmillis = 60000;
+    int numberofwhiteswitchpresses =0;
+    int Numberofblackswitchpresses=0;
+    int number=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextViewCountdown = findViewById( R.id.text_view_countdown);
-        mTextViewCountdown2 = findViewById( R.id.text_view_countdown_2);
+        welcomePage=(ConstraintLayout)findViewById(R.id.welcomePage);
+        clockPage=(ConstraintLayout)findViewById(R.id.clockPage);
+        minutes=(EditText)findViewById(R.id.minutes);
+        playButton=(Button)findViewById(R.id.playButton);
+        startButton=(Button)findViewById(R.id.startButton);
+        whiteCounterTextView=(TextView)findViewById(R.id.whiteCounterTextView);
+        blackCounterTextView=(TextView)findViewById(R.id.blackCounterTextView);
+        wSwitchButton=(Button)findViewById(R.id.whiteSwitchButton);
+        bSwitchButton=(Button)findViewById(R.id.blackSwitchButton);
+        reset=(Button)findViewById(R.id.RestartButton);
 
-        mButtonStartPause = findViewById(R.id.button_start_pause);
-        mButtonStartPause2 = findViewById(R.id.button_start_pause2);
-        mButtonReset = findViewById(R.id.button_reset);
 
-        mButtonStartPause.setOnClickListener(new View.OnClickListener() {
+
+
+        playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mTimer1Running){
+                welcomePage.setVisibility(View.INVISIBLE);
+                clockPage.setVisibility(View.VISIBLE);
+                totalTime=Integer.parseInt(minutes.getText().toString());
+                whiteCounterTextView.setText(totalTime.toString()+":"+"00");
+                blackCounterTextView.setText(totalTime.toString()+":"+"00");
+            }
+        });
 
-                    pauseTimer();
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                whiteCountDownTimer=new CountDownTimer(totalTime*60000,1000) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        wtimeleftinmillis=millisUntilFinished;
+                        int minutes=(int) (millisUntilFinished/1000)/60;
+                        int seconds=(int) (millisUntilFinished/1000)%60;
+                        String timeLeftFormatted = String.format(Locale.getDefault(),"%02d:%02d",minutes,seconds);
+                        whiteCounterTextView.setText(timeLeftFormatted);
+                    }
 
+                    @Override
+                    public void onFinish() {
+                        MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.a);
+                        mp.start();
+                    }
+                }.start();
+                startButton.setVisibility(View.INVISIBLE);
+                wSwitchButton.setVisibility(View.VISIBLE);
+                reset.setVisibility(View.VISIBLE);
+                Numberofblackswitchpresses=1;
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Numberofblackswitchpresses=0;
+                numberofwhiteswitchpresses=0;
+                blackCountDownTimer.cancel();
+                whiteCountDownTimer.cancel();
+                startButton.setVisibility(View.VISIBLE);
+                reset.setVisibility(View.INVISIBLE);
+                wSwitchButton.setVisibility(View.INVISIBLE);
+                bSwitchButton.setVisibility(View.INVISIBLE);
+                whiteCounterTextView.setText(totalTime.toString()+":"+"00");
+                blackCounterTextView.setText(totalTime.toString()+":"+"00");
+            }
+        });
+
+        wSwitchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                whiteCountDownTimer.cancel();
+                if(numberofwhiteswitchpresses>0) {
+                    blackCountDownTimer = new CountDownTimer(btimeleftinmillis, 1000) {
+                        @Override
+                        public void onTick(long millisUntilFinished) {
+                            btimeleftinmillis = millisUntilFinished;
+                            int minutes = (int) (btimeleftinmillis / 1000) / 60;
+                            int seconds = (int) (btimeleftinmillis / 1000) % 60;
+                            String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+                            blackCounterTextView.setText(timeLeftFormatted);
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.a);
+                            mp.start();
+                        }
+                    }.start();
                 }
-                else {
-                    startTimer();
+                else
+                {
+                    numberofwhiteswitchpresses=1;
+                    blackCountDownTimer = new CountDownTimer(totalTime*60000, 1000) {
+                        @Override
+                        public void onTick(long millisUntilFinished) {
+                            btimeleftinmillis = millisUntilFinished;
+                            int minutes = (int) (btimeleftinmillis / 1000) / 60;
+                            int seconds = (int) (btimeleftinmillis / 1000) % 60;
+                            String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+                            blackCounterTextView.setText(timeLeftFormatted);
+                        }
 
+                        @Override
+                        public void onFinish() {
+                            MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.a);
+                            mp.start();
+                        }
+                    }.start();
                 }
+                wSwitchButton.setVisibility(View.INVISIBLE);
+                bSwitchButton.setVisibility(View.VISIBLE);
 
             }
         });
 
-        updateCountDownText();
-        mButtonStartPause2.setOnClickListener(new View.OnClickListener() {
+        bSwitchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mTimer2Running){
-                    pauseTimer2();
-                    startTimer();
+                blackCountDownTimer.cancel();
+                if(Numberofblackswitchpresses>0){
+                    whiteCountDownTimer = new CountDownTimer(wtimeleftinmillis, 1000) {
+                        @Override
+                        public void onTick(long millisUntilFinished) {
+                            wtimeleftinmillis = millisUntilFinished;
+                            int minutes = (int) (wtimeleftinmillis / 1000) / 60;
+                            int seconds = (int) (wtimeleftinmillis / 1000) % 60;
+                            String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+                            whiteCounterTextView.setText(timeLeftFormatted);
+
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.a);
+                            mp.start();
+                        }
+                    }.start();
+
                 }
-                else {
-                    startTimer2();
-                    pauseTimer();
+                else
+                {
+                    Numberofblackswitchpresses=1;
+                    whiteCountDownTimer = new CountDownTimer(totalTime*60000, 1000) {
+                        @Override
+                        public void onTick(long millisUntilFinished) {
+                            wtimeleftinmillis = millisUntilFinished;
+                            int minutes = (int) (wtimeleftinmillis / 1000) / 60;
+                            int seconds = (int) (wtimeleftinmillis / 1000) % 60;
+                            String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+                            whiteCounterTextView.setText(timeLeftFormatted);
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.a);
+                            mp.start();
+                        }
+                    }.start();
                 }
+                bSwitchButton.setVisibility(View.INVISIBLE);
+                wSwitchButton.setVisibility(View.VISIBLE);
 
             }
         });
-        updateCountDownText2();
-        mButtonReset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                resetTimer();
 
-            }
-        });
-    }
-
-    private void startTimer(){
-
-        mCountDownTimer = new CountDownTimer(mTimeLeft1InMillis, 1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                mTimeLeft1InMillis = millisUntilFinished;
-                updateCountDownText();
-            }
-
-            @Override
-            public void onFinish() {
-                mTimer1Running=false;
-                mButtonStartPause.setText("Start 1");
-                mButtonStartPause.setVisibility(View.INVISIBLE);
-                mButtonReset.setVisibility(View.VISIBLE);
-
-            }
-        }.start();
-
-        mTimer1Running= true;
-        mButtonStartPause.setText("START 1");
-        mButtonReset.setVisibility(View.INVISIBLE);
 
     }
-    private void startTimer2(){
-
-        mCountDownTimer2 = new CountDownTimer(mTimeLeft1InMillis, 1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                mTimeLeft2InMillis = millisUntilFinished;
-                updateCountDownText2();
-            }
-
-            @Override
-            public void onFinish() {
-                mTimer1Running=false;
-                mButtonStartPause2.setText("Start 2");
-                mButtonStartPause2.setVisibility(View.INVISIBLE);
-                mButtonReset.setVisibility(View.VISIBLE);
-
-            }
-        }.start();
-
-        mTimer2Running= true;
-        mButtonStartPause.setText("START 1");
-        mButtonReset.setVisibility(View.INVISIBLE);
-
-    }
-
-    private void pauseTimer(){
-        mCountDownTimer.cancel();
-        mTimer1Running=false;
-        mButtonStartPause.setText("Start 1");
-        mButtonReset.setVisibility(View.VISIBLE);
-
-    }
-    private void pauseTimer2(){
-        mCountDownTimer2.cancel();
-        mTimer2Running=false;
-        mButtonStartPause2.setText("Start 2");
-        mButtonReset.setVisibility(View.VISIBLE);
-
-    }
-    private void resetTimer(){
-        mTimeLeft1InMillis= START_TIME_IN_MILLIS;
-        mTimeLeft2InMillis= START_TIME_IN_MILLIS;
-
-        updateCountDownText();
-        updateCountDownText2();
-
-        mButtonReset.setVisibility(View.INVISIBLE);
-        mButtonStartPause.setVisibility(View.VISIBLE);
-        mButtonStartPause2.setVisibility(View.VISIBLE);
-    }
-
-
-    private void updateCountDownText() {
-        int minutes=(int) (mTimeLeft1InMillis/1000)/60;
-        int seconds=(int) (mTimeLeft1InMillis/1000)%60;
-
-        String timeLeftFormatted = String.format(Locale.getDefault(),"%02d:%02d",minutes,seconds);
-
-        mTextViewCountdown.setText(timeLeftFormatted);
-
-    }
-    private void updateCountDownText2() {
-        int minutes=(int) (mTimeLeft2InMillis/1000)/60;
-        int seconds=(int) (mTimeLeft2InMillis/1000)%60;
-
-        String timeLeftFormatted = String.format(Locale.getDefault(),"%02d:%02d",minutes,seconds);
-
-        mTextViewCountdown2.setText(timeLeftFormatted);
-
-    }
-
 }
