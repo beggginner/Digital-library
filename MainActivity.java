54b78626ca06d9689cb5b2a14bd50bc9842b538c
Tharun <53267275+tharun634@users.noreply.github.com>
2020-06-11 11:23:47 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.fkingstop;
+package com.example.timer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,15 +15,19 @@ public class MainActivity extends AppCompatActivity {
     private static final long START_TIME_IN_MILLIS = 600000;
 
     private TextView mTextViewCountdown;
+    private TextView mTextViewCountdown2;
     private Button mButtonStartPause;
+    private Button mButtonStartPause2;
     private Button mButtonReset;
 
     private CountDownTimer mCountDownTimer;
+    private CountDownTimer mCountDownTimer2;
 
-    private boolean mTimerRunning;
+    private boolean mTimer1Running;
+    private boolean mTimer2Running;
 
-    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
-
+    private long mTimeLeft1InMillis = START_TIME_IN_MILLIS;
+    private long mTimeLeft2InMillis = START_TIME_IN_MILLIS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,25 +35,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTextViewCountdown = findViewById( R.id.text_view_countdown);
+        mTextViewCountdown2 = findViewById( R.id.text_view_countdown_2);
 
         mButtonStartPause = findViewById(R.id.button_start_pause);
+        mButtonStartPause2 = findViewById(R.id.button_start_pause2);
         mButtonReset = findViewById(R.id.button_reset);
 
         mButtonStartPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mTimerRunning){
-                    pauseIimer();
+                if(mTimer1Running){
+
+                    pauseTimer();
+
                 }
                 else {
                     startTimer();
+
                 }
 
             }
         });
 
         updateCountDownText();
+        mButtonStartPause2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mTimer2Running){
+                    pauseTimer2();
+                    startTimer();
+                }
+                else {
+                    startTimer2();
+                    pauseTimer();
+                }
 
+            }
+        });
+        updateCountDownText2();
         mButtonReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -62,49 +85,97 @@ public class MainActivity extends AppCompatActivity {
 
     private void startTimer(){
 
-        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+        mCountDownTimer = new CountDownTimer(mTimeLeft1InMillis, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
-                mTimeLeftInMillis = millisUntilFinished;
+                mTimeLeft1InMillis = millisUntilFinished;
                 updateCountDownText();
             }
 
             @Override
             public void onFinish() {
-                mTimerRunning=false;
-                mButtonStartPause.setText("Start");
+                mTimer1Running=false;
+                mButtonStartPause.setText("Start 1");
                 mButtonStartPause.setVisibility(View.INVISIBLE);
                 mButtonReset.setVisibility(View.VISIBLE);
 
             }
         }.start();
 
-        mTimerRunning= true;
-        mButtonStartPause.setText("pause");
+        mTimer1Running= true;
+        mButtonStartPause.setText("START 1");
         mButtonReset.setVisibility(View.INVISIBLE);
 
     }
-    private void pauseIimer(){
+    private void startTimer2(){
+
+        mCountDownTimer2 = new CountDownTimer(mTimeLeft1InMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeft2InMillis = millisUntilFinished;
+                updateCountDownText2();
+            }
+
+            @Override
+            public void onFinish() {
+                mTimer1Running=false;
+                mButtonStartPause2.setText("Start 2");
+                mButtonStartPause2.setVisibility(View.INVISIBLE);
+                mButtonReset.setVisibility(View.VISIBLE);
+
+            }
+        }.start();
+
+        mTimer2Running= true;
+        mButtonStartPause.setText("START 1");
+        mButtonReset.setVisibility(View.INVISIBLE);
+
+    }
+
+    private void pauseTimer(){
         mCountDownTimer.cancel();
-        mTimerRunning=false;
-        mButtonStartPause.setText("Start");
+        mTimer1Running=false;
+        mButtonStartPause.setText("Start 1");
+        mButtonReset.setVisibility(View.VISIBLE);
+
+    }
+    private void pauseTimer2(){
+        mCountDownTimer2.cancel();
+        mTimer2Running=false;
+        mButtonStartPause2.setText("Start 2");
         mButtonReset.setVisibility(View.VISIBLE);
 
     }
     private void resetTimer(){
-        mTimeLeftInMillis= START_TIME_IN_MILLIS;
+        mTimeLeft1InMillis= START_TIME_IN_MILLIS;
+        mTimeLeft2InMillis= START_TIME_IN_MILLIS;
+
         updateCountDownText();
+        updateCountDownText2();
+
         mButtonReset.setVisibility(View.INVISIBLE);
         mButtonStartPause.setVisibility(View.VISIBLE);
+        mButtonStartPause2.setVisibility(View.VISIBLE);
     }
 
+
     private void updateCountDownText() {
-        int minutes=(int) (mTimeLeftInMillis/1000)/60;
-        int seconds=(int) (mTimeLeftInMillis/1000)%60;
+        int minutes=(int) (mTimeLeft1InMillis/1000)/60;
+        int seconds=(int) (mTimeLeft1InMillis/1000)%60;
 
         String timeLeftFormatted = String.format(Locale.getDefault(),"%02d:%02d",minutes,seconds);
 
         mTextViewCountdown.setText(timeLeftFormatted);
+
+    }
+    private void updateCountDownText2() {
+        int minutes=(int) (mTimeLeft2InMillis/1000)/60;
+        int seconds=(int) (mTimeLeft2InMillis/1000)%60;
+
+        String timeLeftFormatted = String.format(Locale.getDefault(),"%02d:%02d",minutes,seconds);
+
+        mTextViewCountdown2.setText(timeLeftFormatted);
+
     }
 
 }
